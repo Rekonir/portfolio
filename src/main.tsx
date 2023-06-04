51841df04e0118355d068865c137ba76c7de2208
Rekonir <zhed-nikita@mail.ru>
2023-06-04 21:02:20 +0300

    some fix page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App/App.tsx'
+import { HashRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-
-  <App />
+  <HashRouter><App /></HashRouter>,
 
 )
