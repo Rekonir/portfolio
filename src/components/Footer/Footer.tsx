8ab1f0f7eff7e997f3fd51d9677b5617ae545d83
Rekonir <zhed-nikita@mail.ru>
2023-06-06 11:33:30 +0300

    add link to hh.ru

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,12 +8,16 @@ const Footer = () => {
             <h3>Experience </h3>
             <Stack />
             <div className={style.btns}>
-                <Button variant='white'>
-                    Скачать резюме
-                </Button>
-                <Button variant="red">
-                    Я на hh.ru
-                </Button>
+                <a href='https://bryansk.hh.ru/resume_converter/Жедь%20Никита%20Владимирович.pdf?hash=2cc743f9ff0bfe0c010039ed1f456965696d36&type=pdf&hhtmSource=resume&hhtmFrom=resume_builder_position'>
+                    <Button variant='white' >
+                        Скачать резюме
+                    </Button>
+                </a>
+                <a href='https://bryansk.hh.ru/applicant/resumes/view?resume=2cc743f9ff0bfe0c010039ed1f456965696d36'>
+                    <Button variant="red">
+                        Я на hh.ru
+                    </Button>
+                </a>
             </div>
         </div>
     );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,29 +3,22 @@ import Button from '../UI/Button/Button';
 
 
 const Header = () => {
-    // const downloadFile = () => {
-    //     const fileUri =
-    //         "https://example.com/example.docx";
-
-    //     const link = document.createElement("a");
-    //     link.href = fileUri;
-    //     link.setAttribute("download", "example.docx");
-    //     document.body.appendChild(link);
-    //     link.click();
-    //     document.body.removeChild(link);
-    // };
     return (
         <div className={style.header}>
             <div className={style.contacts}>
                 <p>Телефон для связи: +7(981)973-80-72</p>
                 <p>Почта для связи: zhed-nikita@mail.ru</p>
             </div>
-            <Button variant='white' >
-                Скачать резюме
-            </Button>
-            <Button variant="red" >
-                Я на hh.ru
-            </Button>
+            <a href='https://bryansk.hh.ru/resume_converter/Жедь%20Никита%20Владимирович.pdf?hash=2cc743f9ff0bfe0c010039ed1f456965696d36&type=pdf&hhtmSource=resume&hhtmFrom=resume_builder_position'>
+                <Button variant='white' >
+                    Скачать резюме
+                </Button>
+            </a>
+            <a href='https://bryansk.hh.ru/applicant/resumes/view?resume=2cc743f9ff0bfe0c010039ed1f456965696d36'>
+                <Button variant="red">
+                    Я на hh.ru
+                </Button>
+            </a>
         </div>
     );
 };
